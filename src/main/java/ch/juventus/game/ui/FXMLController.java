dda83c8dd2e424d4230cb41bb27163ef65add629
Jan Minder <minderjan@gmail.com>
2019-04-07 18:40:33 +0200

    Implement OpenJavaFX Support
    * Add JavaFX Gradle Plugin
    * Update README
    * Add FXML Controller
    * Add Logging 4 Java Library
    * Add Logback Configuration

---
+++ src/main/java/ch/juventus/game/ui/FXMLController.java
@@ -0,0 +1,27 @@
+package ch.juventus.game.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLController implements Initializable {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @FXML
+    private Label label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        LOGGER.debug("Initialize FXML Controller");
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    }
+
+}
